0e3fe7eb16c0b3010a025e2f8f822fef138c16d4
Wong Wai Kit <21130151+kitwong607@users.noreply.github.com>
2021-10-22 08:03:33 +0800

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,52 @@
+import React, { Suspense } from "react";
+import {toAbsoluteUrl} from './helpers';
+import { useTranslation } from 'react-i18next';
+import { Trans } from "react-i18next";
+
+const MyComponent = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+     i18n.changeLanguage(lng);
+  };
+
+  return (
+    <Suspense fallback="loading">
+     <div className="App">
+        <header className="App-header">
+           <h1>{t("hello")}</h1>
+           <img src={toAbsoluteUrl("media/svg/logo.svg")} className="App-logo" alt="logo" />
+           <Trans i18nKey="link">
+              一起來
+              <a
+                 className="App-link"
+                 href="https://reactjs.org"
+                 target="_blank"
+                 rel="noopener noreferrer"
+              >
+                 這裡
+              </a>
+              學習 React 吧
+           </Trans>
+           <div>
+              <button onClick={() => changeLanguage("tw")}>中文</button>
+              <button onClick={() => changeLanguage("en")}>English</button>
+              <button onClick={() => changeLanguage("es")}>Español</button>
+           </div>
+        </header>
+     </div>
+      </Suspense>
+  );
+};
+
+
+function App() {
+
+  return (
+    
+      <MyComponent /> 
+    
+  );
+}
+
+export default App;
